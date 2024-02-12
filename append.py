8e4163fc25b0d98581a09621373af98b81a00a30
Solina <158811171+SolinaChhem@users.noreply.github.com>
2024-02-12 08:46:48 +0700

    Add files via upload

---
+++ append.py
@@ -0,0 +1,8 @@
+shopping_list = ["apple", "fish","mangos"] 
+
+print("Before append" , shopping_list)
+shopping_list.append("NEW ITEM")
+print("After append" , shopping_list)
+
+shopping_list.remove("apple")
+print("After remove apple", shopping_list)
---
+++ loop.py
@@ -0,0 +1,23 @@
+#my_list = [1,2,3,4,5]
+
+#for i in my_list:
+    #print(i) 
+
+# i = 1
+# i = 2
+# i = 3
+# i = 4
+# i = 5
+    
+#counter = 0
+    
+#while counter <10:
+    #print(counter) 
+    #counter += 1 
+
+while True:
+    option = input("Enter character: ")
+    if option == "q":
+        break
+    elif option == "a":
+        print("You type a")
